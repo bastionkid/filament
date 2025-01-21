0f6cbb10860c917492452bcda7af3b1fccad9505
bastionkid <akash.mercer@gmail.com>
2025-01-21 15:57:23 +0530

    RenderUtils added

---
+++ android/samples/sample-gltf-viewer/src/main/java/com/google/android/filament/gltf/RenderUtils.kt
@@ -0,0 +1,34 @@
+package com.google.android.filament.gltf
+
+import com.google.android.filament.Entity
+import com.google.android.filament.RenderableManager
+
+/**
+ * Shows an entity.
+ *
+ * @see com.google.android.filament.View.setVisibleLayers
+ */
+fun RenderableManager.showEntity(@Entity entity: Int): Boolean {
+    return if (hasComponent(entity)) {
+        // (1: visible, 0: invisible)
+        setLayerMask(getInstance(entity), 0xFF, 1)
+        true
+    } else {
+        false
+    }
+}
+
+/**
+ * Hides an entity.
+ *
+ * @see com.google.android.filament.View.setVisibleLayers
+ */
+fun RenderableManager.hideEntity(@Entity entity: Int): Boolean {
+    return if (hasComponent(entity)) {
+        // (1: visible, 0: invisible)
+        setLayerMask(getInstance(entity), 0xFF, 0)
+        true
+    } else {
+        false
+    }
+}

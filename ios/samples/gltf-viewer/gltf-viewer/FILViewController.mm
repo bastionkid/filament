f730cb816400a2c541bc2b6ba687e5d290e7337b
bastionkid <akash.mercer@gmail.com>
2025-01-29 22:37:52 +0530

    ios - redundant code removed

--- ios/samples/gltf-viewer/gltf-viewer/FILViewController.mm
+++ ios/samples/gltf-viewer/gltf-viewer/FILViewController.mm
@@ -55,26 +55,18 @@ const float kToastDelayDuration = 2.0f;
 @implementation FILViewController {
     CADisplayLink* _displayLink;
     CFTimeInterval _startTime;
-    viewer::RemoteServer* _server;
-    viewer::AutomationEngine* _automation;
-    UILabel* _toastLabel;
 
     Texture* _skyboxTexture;
     Skybox* _skybox;
     Texture* _iblTexture;
     IndirectLight* _indirectLight;
     Entity _sun;
-
-    UITapGestureRecognizer* _singleTapRecognizer;
-    UITapGestureRecognizer* _doubleTapRecognizer;
 }
 
 #pragma mark UIViewController methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.title = @"https://google.github.io/filament/remote";
 
     // Observe lifecycle notifications to prevent us from rendering in the background.
     [NSNotificationCenter.defaultCenter addObserver:self
@@ -86,56 +78,10 @@ const float kToastDelayDuration = 2.0f;
                                                name:UIApplicationDidBecomeActiveNotification
                                              object:nil];
 
-    // Arguments:
-    // --model <path>
-    //     path to glb or gltf file to load from documents directory
-    NSString* modelPath = nil;
-
-    NSArray* arguments = [[NSProcessInfo processInfo] arguments];
-    for (NSUInteger i = 0; i < arguments.count; i++) {
-        NSString* argument = arguments[i];
-        NSString* nextArgument = (i + 1) < arguments.count ? arguments[i + 1] : nil;
-        if ([argument isEqualToString:@"--model"]) {
-            if (!nextArgument) {
-                NSLog(@"Warning: --model option requires path argument. None provided.");
-            }
-            modelPath = nextArgument;
-        }
-    }
-
-    if (modelPath) {
-        [self createRenderablesFromPath:modelPath];
-    } else {
-        [self createDefaultRenderables];
-    }
+    [self createDefaultRenderables];
+    
+    // Creating light is mandatory
     [self createLights];
-
-    _server = new viewer::RemoteServer();
-    _automation = viewer::AutomationEngine::createDefault();
-
-    _doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                                   action:@selector(reloadModel)];
-    _doubleTapRecognizer.numberOfTapsRequired = 2;
-    [self.modelView addGestureRecognizer:_doubleTapRecognizer];
-    _singleTapRecognizer =
-            [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                    action:@selector(issuePickingQuery)];
-    _singleTapRecognizer.numberOfTapsRequired = 1;
-    [self.modelView addGestureRecognizer:_singleTapRecognizer];
-
-    // Create a label at the top of the screen to toast messages to the user.
-    CGRect labelRect = self.view.bounds;
-    labelRect.size.height = 50;
-    _toastLabel = [[UILabel alloc] initWithFrame:labelRect];
-    _toastLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    _toastLabel.textAlignment = NSTextAlignmentCenter;
-    _toastLabel.textColor = [UIColor whiteColor];
-    _toastLabel.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
-    _toastLabel.numberOfLines = 0;
-    _toastLabel.lineBreakMode = NSLineBreakByWordWrapping;
-    _toastLabel.text = @"";
-    _toastLabel.alpha = 0.0f;
-    [self.view addSubview:_toastLabel];
 }
 
 - (void)appWillResignActive:(NSNotification*)notification {
@@ -171,73 +117,28 @@ const float kToastDelayDuration = 2.0f;
     _displayLink = nil;
 }
 
-- (void)createRenderablesFromPath:(NSString*)model {
-    // Retrieve the full path to the model in the documents directory.
-    NSString* documentPath = [NSSearchPathForDirectoriesInDomains(
-            NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString* path = [documentPath stringByAppendingPathComponent:model];
-
-    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        NSLog(@"Error: no file exists at %@", path);
-        return;
-    }
-
-    NSData* buffer = [NSData dataWithContentsOfFile:path];
-    if ([model hasSuffix:@".glb"]) {
-        [self.modelView loadModelGlb:buffer];
-    } else if ([model hasSuffix:@".gltf"]) {
-        NSString* parentDirectory = [path stringByDeletingLastPathComponent];
-        [self.modelView loadModelGltf:buffer
-                             callback:^NSData*(NSString* uri) {
-                                 NSString* p = [parentDirectory stringByAppendingPathComponent:uri];
-                                 return [NSData dataWithContentsOfFile:p];
-                             }];
-    } else {
-        NSLog(@"Error: file %@ must have either a .glb or .gltf extension.", path);
-        return;
-    }
-
-    self.title = model;
-    [self.modelView transformToUnitCube];
-}
-
 - (void)createDefaultRenderables {
-    NSString* path = [[NSBundle mainBundle] pathForResource:@"scene"
-                                                     ofType:@"gltf"
-                                                inDirectory:@"BusterDrone"];
-    assert(path.length > 0);
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"stadium" ofType:@"gltf" inDirectory:@"BusterDrone"];
     NSData* buffer = [NSData dataWithContentsOfFile:path];
-    [self.modelView loadModelGltf:buffer
-                         callback:^NSData*(NSString* uri) {
-                             NSString* p = [[NSBundle mainBundle] pathForResource:uri
-                                                                           ofType:@""
-                                                                      inDirectory:@"BusterDrone"];
-                             return [NSData dataWithContentsOfFile:p];
-                         }];
+    [self.modelView loadModelGlb:buffer];
     [self.modelView transformToUnitCube];
 }
 
 - (void)createLights {
     // Load Skybox.
-    NSString* skyboxPath = [[NSBundle mainBundle] pathForResource:@"default_env_skybox"
-                                                           ofType:@"ktx"];
-    assert(skyboxPath.length > 0);
+    NSString* skyboxPath = [[NSBundle mainBundle] pathForResource:@"default_env_skybox" ofType:@"ktx"];
     NSData* skyboxBuffer = [NSData dataWithContentsOfFile:skyboxPath];
 
-    image::Ktx1Bundle* skyboxBundle =
-            new image::Ktx1Bundle(static_cast<const uint8_t*>(skyboxBuffer.bytes),
-                    static_cast<uint32_t>(skyboxBuffer.length));
+    image::Ktx1Bundle* skyboxBundle = new image::Ktx1Bundle(static_cast<const uint8_t*>(skyboxBuffer.bytes), static_cast<uint32_t>(skyboxBuffer.length));
     _skyboxTexture = Ktx1Reader::createTexture(self.modelView.engine, skyboxBundle, false);
     _skybox = filament::Skybox::Builder().environment(_skyboxTexture).build(*self.modelView.engine);
     self.modelView.scene->setSkybox(_skybox);
 
     // Load IBL.
     NSString* iblPath = [[NSBundle mainBundle] pathForResource:@"default_env_ibl" ofType:@"ktx"];
-    assert(iblPath.length > 0);
     NSData* iblBuffer = [NSData dataWithContentsOfFile:iblPath];
 
-    image::Ktx1Bundle* iblBundle = new image::Ktx1Bundle(
-            static_cast<const uint8_t*>(iblBuffer.bytes), static_cast<uint32_t>(iblBuffer.length));
+    image::Ktx1Bundle* iblBundle = new image::Ktx1Bundle(static_cast<const uint8_t*>(iblBuffer.bytes), static_cast<uint32_t>(iblBuffer.length));
     math::float3 harmonics[9];
     iblBundle->getSphericalHarmonics(harmonics);
     _iblTexture = Ktx1Reader::createTexture(self.modelView.engine, iblBundle, false);
@@ -259,31 +160,6 @@ const float kToastDelayDuration = 2.0f;
     self.modelView.scene->addEntity(_sun);
 }
 
-- (void)loadSettings:(viewer::ReceivedMessage const*)message {
-    viewer::AutomationEngine::ViewerContent content = {
-        .view = self.modelView.view,
-        .renderer = self.modelView.renderer,
-        .materials = nullptr,
-        .materialCount = 0u,
-        .lightManager = &self.modelView.engine->getLightManager(),
-        .scene = self.modelView.scene,
-        .indirectLight = _indirectLight,
-        .sunlight = _sun,
-    };
-    _automation->applySettings(
-            self.modelView.engine, message->buffer, message->bufferByteCount, content);
-    ColorGrading* const colorGrading = _automation->getColorGrading(self.modelView.engine);
-    self.modelView.view->setColorGrading(colorGrading);
-    self.modelView.cameraFocalLength = _automation->getViewerOptions().cameraFocalLength;
-}
-
-- (void)loadGlb:(viewer::ReceivedMessage const*)message {
-    [self.modelView destroyModel];
-    NSData* buffer = [NSData dataWithBytes:message->buffer length:message->bufferByteCount];
-    [self.modelView loadModelGlb:buffer];
-    [self.modelView transformToUnitCube];
-}
-
 - (void)render {
     auto* animator = self.modelView.animator;
     if (animator) {
@@ -294,66 +170,11 @@ const float kToastDelayDuration = 2.0f;
         animator->updateBoneMatrices();
     }
 
-    // Check if a new message has been fully received from the client.
-    viewer::ReceivedMessage const* message = _server->acquireReceivedMessage();
-    if (message && message->label) {
-        NSString* label = [NSString stringWithCString:message->label encoding:NSUTF8StringEncoding];
-        if ([label hasSuffix:@".json"]) {
-            [self loadSettings:message];
-        } else if ([label hasSuffix:@".glb"]) {
-            self.title = label;
-            [self loadGlb:message];
-        }
-
-        _server->releaseReceivedMessage(message);
-    }
-
     [self.modelView render];
-}
-
-- (void)reloadModel {
-    [self.modelView destroyModel];
-    [self createDefaultRenderables];
-}
-
-- (void)issuePickingQuery {
-    CGPoint tapLocation = [_singleTapRecognizer locationInView:self.modelView];
-    __weak typeof(self) weakSelf = self;
-    [self.modelView issuePickQuery:tapLocation
-                          callback:^(utils::Entity entity) {
-                              NSString* name = [self.modelView getEntityName:entity];
-                              if (!name) {
-                                  name = @"<unnamed>";
-                              }
-                              NSString* message = [NSString
-                                      stringWithFormat:@"Picked entity %d (%@) at (%d,%d)",
-                                      entity.getId(), name, int(tapLocation.x), int(tapLocation.y)];
-                              [weakSelf toastMessage:message];
-                          }];
-}
-
-- (void)toastMessage:(NSString*)message {
-    _toastLabel.text = message;
-    _toastLabel.alpha = 0.0f;
-    [UIView animateWithDuration:kToastAnimationDuration
-            animations:^{
-                _toastLabel.alpha = 1.0f;
-            }
-            completion:^(BOOL finished) {
-                [UIView animateWithDuration:kToastAnimationDuration
-                                      delay:kToastDelayDuration
-                                    options:UIViewAnimationOptionCurveEaseInOut
-                                 animations:^{
-                                     _toastLabel.alpha = 0.0f;
-                                 }
-                                 completion:nil];
-            }];
 }
 
 - (void)dealloc {
     [NSNotificationCenter.defaultCenter removeObserver:self];
-    delete _server;
-    delete _automation;
     self.modelView.engine->destroy(_indirectLight);
     self.modelView.engine->destroy(_iblTexture);
     self.modelView.engine->destroy(_skybox);
